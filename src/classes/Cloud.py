1c7319da59ac827236ea4c7d0191efc8f17261e4
pstetz <pstetz@berkeley.edu>
2019-08-18 18:42:30 -0700

    working DicomDir class

--- src/classes/Cloud.py
+++ src/classes/Cloud.py
@@ -6,22 +6,22 @@ class Cloud:
     def __init__(self):
         API_KEY = os.environ["FLYWHEEL_API"]
         self.fw = flywheel.Flywheel(API_KEY)
-        
+
     """
     Downloading
     """
     def download(self, project, subject, time_session, task, output_directory="/Users/pbezuhov/tmp"):
         ### Find Flwheel project
         fw_project = [p for p in self.fw.get_all_projects() if p.label == project][0]
-        
+
         ### Get all valid sessions for project
         sessions     = self.fw.get_project_sessions(fw_project.id)
         sessions     = [s for s in sessions if s.label.isdigit()]
-        
+
         ### Filter by subject and time session
         sessions = self._filter_subject(sessions, subject)
         sessions = self._filter_time_session(sessions, time_session)
-        
+
         ### Find unique task
         ### (NOTE: mark the task as donotuseme if there are multiple)
         acq_ids = self._get_task_files(sessions, task)
@@ -30,13 +30,13 @@ class Cloud:
         if len(acq_ids) != 1:
             raise Exception("Multiple task %s found for %s %s %s on Flwheel" % (task, project, subject, time_session))
         acq_id = acq_ids.pop()
-        
+
         ### Download the task dicom
         output_path = join(output_directory, "%s-%s-%s-%s.dicom.zip" % (project, subject, time_session, task))
         dicom_filename = self._get_dicom_filename(acq_id)
         self._download_file(acq_id, dicom_filename, output_path)
         return output_path
-    
+
     def _download_file(self, acq_id, filename, output_path):
         if os.path.isfile(output_path):
             print("%s already exists!  Overwriting..." % output_path)
@@ -50,18 +50,18 @@ class Cloud:
         print("Downloading %s..." % output_path)
         self.fw.download_file_from_acquisition(acq_id, filename, output_path)
 
-        
+
     """
     Project
     """
     def _find_project_label(self, session):
         project_id = session.project
         return self.fw.get_project(project_id).label
-    
-    
+
+
     """
     Subject
-    """    
+    """
     def _determine_subject(self, session):
         subject = session.subject.code
         subject = subject.lower()
@@ -72,13 +72,13 @@ class Cloud:
             return subject.split("_")[0]
         elif project == "engage":
             return subject[:len("LA13012")]
-    
+
     def _filter_subject(self, sessions, subject):
         subject = subject.lower()
         sessions = [s for s in sessions if self._determine_subject(s) == subject]
         return sessions
 
-    
+
     """
     Time session
     """
@@ -108,7 +108,7 @@ class Cloud:
                 session = subject_id.split("-")[1]
                 return session_map[session]
         raise Exception("Project %s not known" % project)
-            
+
     def _fw_time_session(self, session_id):
         """
         It's very important to get the tags from this method.  If you pass in a session from
@@ -124,12 +124,12 @@ class Cloud:
             raise Exception("Multiple time sessions (%s) for session ID %s." % (", ".join(list(time_session)), session_id))
         else:
             return "%s_data_archive" % (time_session.pop())
-    
+
     def _filter_time_session(self, sessions, time_session):
         sessions = [s for s in sessions if self._determine_time_session(s) == time_session]
         return sessions
 
-    
+
     """
     Tasks
     """
@@ -140,14 +140,14 @@ class Cloud:
                 if self._is_match(acq.label, task_name):
                     task_files.append(acq.id)
         return task_files
-    
+
     def _is_match(self, fw_label, task_name):
         fw_label = fw_label.lower().replace(" ", "_").replace("-", "_")
-        
+
         ### Invalid files are never a match
         if "donotuse" in fw_label or "nope" in fw_label:
             return False
-        
+
         ### Check if file matches
         if task_name == "gonogo" and "go_no_go" in fw_label:
             return True
@@ -156,10 +156,10 @@ class Cloud:
         elif task_name == "nonconscious" and "nonconscious" in fw_label:
             return True
         return False
-    
+
     def _get_dicom_filename(self, acq_id):
         acq = self.fw.get_acquisition(acq_id)
         dicom = [f.name for f in acq.files if f.name.endswith(".dicom.zip")]
         assert len(dicom) == 1, "Multiple dicoms found"
         return dicom.pop()
-    
+
