f1db42193a4e92cfe23097e0a4b43e05a43410b2
pstetz <pstetz@berkeley.edu>
2019-08-25 08:49:53 -0700

    class for patient infomation

---
+++ src/classes/Patient.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from os.path import join
+
+class Subject:
+    def __init__(self, project, subject_id):
+        self.project = project
+        self.subject = subject_id
+        df = self._get_df()
+        self.get_age(df)
+        self.get_sex(df)
+
+    def _get_df(self):
+        data_root = "../../data/project"
+        csv_loc = {
+                "rad": join(data_root, "rad", "rad.csv"),
+                "engage": join(data_root, "engage", "engage.csv"),
+                "connectome": join(data_root, "connectome", "connectome.csv"),
+                }
+        assert self.project in csv_loc, "Project %s does not have patient info" % self.project
+        csv = patient_info[self.project]
+        return pd.read_csv(csv)
+
+    def _get_subject(self, df):
+        """ Returns a JSON of the subject information """
+        subject = df[df["subject"] == self.subject]
+        assert len(subject) != 0, "Subject %s not found" % self.subject
+        assert len(subject) == 1, "Multiple subjects with id %s found" % self.subject
+        info = subject.to_dict()
+        info = {k: info[k][0] for k in info}
+        return info
+
+    def get_age(self, df):
+        info = self._get_subject(df)
+        age  = info["age"]
+        self.age = age
+        return age
+
+    def get_sex(self, df):
+        info = self._get_subject(df)
+        age  = info["age"]
+        self.age = age
+        return age
+
