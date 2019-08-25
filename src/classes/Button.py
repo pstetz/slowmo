b770e1bc993189474716e0f99332fa32b51035f8
pstetz <pstetz@berkeley.edu>
2019-08-25 10:32:37 -0700

    engage patient data quick loog

--- src/classes/Button.py
+++ src/classes/Button.py
@@ -7,19 +7,19 @@ class Button:
         self.studies = studies
         self.buttons = list()
         self.find_logs()
-    
+
     def find_all_logs(self):
         for study in studies:
             study_root = self.get_study_root(study)
             self.find_study_logs(study, study_path)
-            
+
     def find_study_logs(self, study_path):
         for subject_path in glob(join(study_path, "*")):
             subject = os.path.basename(subject_path)
             for time_session_path in glob(join(subject_path, "*")):
                 time_session = os.path.basename(time_session_path)
                 self.find_subject_session_logs(self, study, subject, time_session, time_session_path)
-                
+
     def find_subject_session_logs(self, study, subject, time_session, time_session_path):
         fmri_folder = join(time_session_path, "100_fMRI")
         task_paths  = glob(join(fmri_folder, "10*"))
@@ -27,7 +27,7 @@ class Button:
             task = self.determine_task_name(task_path)
             onsets = Onsets(task_path, task, study, subject, time_session)
             self.buttons.append(onsets)
-    
+
     def _get(self, study=None, subject_id=None, time_session=None, task=None):
         buttons = self.buttons
         buttons = [button for button in button if button.valid]
@@ -40,31 +40,32 @@ class Button:
         if task:
             buttons = [button for button in buttons if button.task == task]
         return buttons
-        
+
     def get_study_root(self, study):
+        data_root = "/Volumes/group/PANLab_Datasets"
         study_path_map = {
-            "RAD": "/Volumes/group/PANLab_Datasets/RAD/data",
-            "ENGAGE": "/Volumes/group/PANLab_Datasets/ENGAGE/data",
-            "ENGAGE_2": "/Volumes/group/PANLab_Datasets/ENGAGE_2/data",
-            "CONN_HC": "/Volumes/group/PANLab_Datasets/CONNECTOME/conn_hc/data",
-            "CONN_MDD": "/Volumes/group/PANLab_Datasets/CONNECTOME/conn_mdd/data",
+            "RAD": join(data_root, "RAD", "data"),
+            "ENGAGE": join(data_root, "ENGAGE", "data"),
+            "ENGAGE_2": join(data_root, "ENGAGE_2", "data"),
+            "CONN_HC": join(data_root, "CONNECTOME", "conn_hc", "data"),
+            "CONN_MDD": jojin(data_root, "CONNECTOME", "conn_mdd", "data"),
         }
         return study_path_map[study]
-    
-    
+
+
 class Onsets:
     def __init__(self, onset_folder, task):
         self.onset_folder = onset_folder
         self.task = task
-        
+
         self.find_onsets()
         self.check_onsets()
-        
+
     def find_onsets(self):
         onsets = glob(join(self.onset_folder, "*.csv"))
         self.onset_paths = [ons for ons in onsets]
         return self.onset_paths
-        
+
     def check_onsets(self):
         if self.task == "gonogo":
             self.valid = (len(self.onset_paths) == 2)
