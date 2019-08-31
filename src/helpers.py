2b82bc53c29e1f233d4e65cb8a87c679de3bcc54
pstetz <pstetz@berkeley.edu>
2019-08-30 20:48:34 -0700

    helpers to format data for nn

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def voxel_radius(radius):
     valid = list()
     for i in range(radius+1):
@@ -30,3 +32,52 @@ def _valid_patient(patient, dicom_path, button, logger):
         logger.log()
         valid = False
     return valid
+
+def _train_task(project, task, onsets, data, patient, is_ascending):
+    for num_volume in len(data.shape[-1]):
+        df = _gen_data(project, task, onsets, volume, patient, is_ascending, num_volume)
+
+def _volume_data(data, x, y, z, t, direction, radius):
+    row = dict()
+    for coord in voxel_radius(radius):
+        i, j, k  = coord["x"], coord["y"], coord["z"]
+        loc      = "i%d_j%d_k%d_t%d" % (i, j, k, direction)
+        row[loc] = data[x+i, y+j ,z+k, t+direction]
+    return row
+
+def _gen_avail_volumes(shape, radius):
+    x_min, x_max = radius, shape[0] - radius
+    y_min, y_max = radius, shape[1] - radius
+    z_min, z_max = radius, shape[2] - radius
+    return [(x, y, z) for x in range(x_min, x_max)
+                      for y in range(y_min, y_max)
+                      for z in range(z_min, z_max)
+           ]
+
+def _gen_data(project, task, onsets, dicoms, patient, is_ascending, num_volume, radius=5):
+    avail_volumes = _gen_avail_volumes(dicoms.shape)
+    for x, y, z in avail_volumes:
+        _prev = _volume_data(dicoms, x, y, z, num_volume, -1, radius)
+        _next = _volume_data(dicoms, x, y, z, num_volume,  1, radius)
+        row = {**_prev, **_next}
+        row["signal"] = data[x, y, z, num_volume]
+    df = pd.DataFrame(rows)
+
+    ### patient data
+#     df["age"] = patient.age
+#     df["is_female"] = (patient.gender == "female")
+
+    ### Project data
+    df["is_rad"]        = (project == "rad")
+    df["is_engage"]     = (project == "engage")
+    df["is_connectome"] = (project == "connectome")
+
+    ### task data
+    df["is_gng"] = (task == "gonogo")
+    df["is_fc"]  = (task == "conscious")
+    df["is_fnc"] = (task == "nonconscious")
+
+    ### fMRI data
+    df["ascending"] = int(is_ascending)
+    return df
+
