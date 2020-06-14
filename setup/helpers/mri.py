c16f1c602653c6db7771abf6992f09d1a68f255e
pstetz <pstetz@PBS-C02V53CKJ1GN.local>
2020-06-14 11:14:59 -0700

    refactor normalize script

---
+++ setup/helpers/mri.py
@@ -0,0 +1,53 @@
+import numpy as np
+import nibabel as nib
+from os.path import isfile
+
+def load_image(filepath):
+    return nib.as_closest_canonical(nib.load(filepath))
+
+def get_data(image):
+    if type(image) == str and isfile(image):
+        return load_image(image).get_data()
+    return image.get_data()
+
+def std_image(data, axis=3):
+    """ Gets the std of the image over time """
+    return np.std(data, axis=axis)
+
+def mean_image(data, axis=3):
+    """ Gets the mean of the image over time """
+    return np.mean(data, axis=axis)
+
+def save_image(data, save_path, affine=None):
+    assert not affine is None, "Affine is not specified"
+    image = nib.Nifti1Image(data, affine)
+    nib.save(image, save_path)
+
+def norm_image(data, mask_data):
+    """ Subtracts the mean and divides by the std """
+    std_data  = std_image(data)
+    mean_data = mean_image(data)
+    norm_data = np.zeros(data.shape)
+    for t in range(norm_data.shape[3]):
+        ### Surpress errors from dividing by 0, Inf, or NaN
+        ### https://stackoverflow.com/a/23116937/9104642
+        with np.errstate(divide='ignore', invalid='ignore'):
+            norm_data[:, :, :, t] = np.divide(
+                    np.multiply(
+                        np.subtract(
+                            data[:, :, :, t],
+                            mean_data),
+                        mask_data
+                    ),
+                    std_data
+                )
+    norm_data[np.isnan(norm_data)] = 0
+    return norm_data.astype(np.float32)
+
+def nii_region(data, x, y, z, r = 4, shape="square"):
+    assert shape == "square" # A ball would be helpful too
+    return data[
+        x - r : x + r + 1,
+        y - r : y + r + 1,
+        z - r : z + r + 1
+    ]
--- setup/normalize.py
+++ setup/normalize.py
@@ -1,68 +1,33 @@
 import os
-import numpy as np
-import nibabel as nib
 from glob import glob
 from tqdm import tqdm
-from os.path import join
+from os.path import basename, isfile, join
+from helpers.mri import load_image, save_image, get_data, norm_image
 
-def _norm(data, mask_data):
-    """ Subtracts the mean and divides by the std """
-    std_data  = _std(data)
-    mean_data = _mean(data)
-    norm_data = np.zeros(data.shape)
-    for t in range(norm_data.shape[3]):
-        ### Surpress errors from dividing by 0, Inf, or NaN
-        ### https://stackoverflow.com/a/23116937/9104642
-        with np.errstate(divide='ignore', invalid='ignore'):
-            norm_data[:, :, :, t] = np.divide(
-                    np.multiply(
-                        np.subtract(
-                            data[:, :, :, t],
-                            mean_data),
-                        mask_data
-                    ),
-                    std_data
-                )
-    norm_data[np.isnan(norm_data)] = 0
-    return norm_data.astype(np.float32)
-
-def _normalize(src, dst, mask):
+def normalize_over_time(src, dst):
     """ Normalizes and saves the brain image """
+    mask = "/Volumes/hd_4tb/slowmo/data/masks/mni/MNI152_T1_2mm_brain_mask.nii.gz"
 
-    ### Load data
-    image  = nib.load(src)
-    affine = image.affine
-    data   = image.get_data()
+    image  = load_image(src)
+    data   = get_data(image)
+    mask_data = get_data(mask)
 
-    ### Calculate the normalized data
-    mask_data = nib.load(mask).get_data()
-    norm_data = _norm(data, mask_data)
+    norm_data = norm_image(data, mask_data)
+    save_image(norm_data, dst, affine=image.affine)
 
-    ### Create and save norm image
-    norm_image = nib.Nifti1Image(norm_data, affine)
-    nib.save(norm_image, dst)
-
-def normalize_project(root, project, mask):
-    """ Normalizes all warped.nii.gz images in a project """
-    for time_path in glob(join(root, project, "*")):
-        for subject_path in tqdm(glob(join(time_path, "*"))):
-            for task_path in glob(join(subject_path, "*")):
-                src = join(task_path, "warped.nii.gz")
-                dst = join(task_path, "normalized.nii.gz")
-                if not os.path.isfile(src):
-                    continue
-                if os.path.isfile(dst):
-                    continue
-                try:
-                    _normalize(src, dst, mask)
-                except Exception as e:
-                    print(e)
-                    print("src", src)
-                    print("dst", dst)
+def normalize_project(root):
+    for subject_path in tqdm(glob(join(root, "*"))):
+        for task_path in glob(join(subject_path, "func", "*")):
+            tail = "_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz"
+            matches = glob(join(task_path, f"*{tail}"))
+            for src in matches:
+                filename = basename(src).replace(tail, "_standardized.nii.gz")
+                filename = "_".join(filename.split("_")[1:]) # remove subject info
+                dst = join(task_path, filename)
+                if isfile(dst): continue
+                normalize_over_time(src, dst)
 
 
 if __name__ == "__main__":
-    root = "/Volumes/hd_4tb/raw"
-    mask = "/Volumes/hd_4tb/masks/mni/MNI152_T1_2mm_brain_mask.nii.gz"
-    for project in projects:
-        normalize_project(root, project, mask)
+    root = "/Volumes/hd_4tb/slowmo/data/fmri/connectome"
+    normalize_project(root)
