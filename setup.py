0fbf3117188e841086e7bad8c43178bd9c2f1138
pstetz <pstetz@berkeley.edu>
2020-07-03 13:05:30 -0700

    package repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="slowmo",
+      version="0.1",
+      url="https://github.com/pstetz/slowmo",
+      author="Patrick Stetz",
+      packages=["utils"],
+      zip_safe=False)
+
+
