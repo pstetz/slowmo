7d96794122a423d9b60b7a60ece99502f3989665
pstetz <pstetz@berkeley.edu>
2020-07-09 11:06:29 -0700

    allow setup to import custom scripts

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="slowmo",
       version="0.1",
       url="https://github.com/pstetz/slowmo",
       author="Patrick Stetz",
-      packages=["slowmo"],
+      packages=find_packages(),
       zip_safe=False)
 
 
