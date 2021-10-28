2215a5f9575533c6d90a20f76df8e6b5ceeb4602
Patrick Stetz <pstetz@sh03-ln05.stanford.edu>
2021-10-28 14:28:51 -0700

    create get_folders helper function

--- slowmo/setup/simple.py
+++ slowmo/setup/simple.py
@@ -5,18 +5,26 @@ from glob import glob
 from tqdm import tqdm
 from os.path import basename, dirname, isdir, isfile, join
 
+def get_folders(root):
+    folders = list()
+    for instance in tqdm(glob(join(root, "*"))):
+        for run in range(10):
+            folder = join(instance, "%02d" % run)
+            assert isfile(join(folder, "info.csv")), folder
+            folders.append(folder)
+    return folders
+
 def collate(root):
     dfs = list()
-    for instance in tqdm(glob(join(root, "*"))):
-        for run in glob(join(instance, "*")):
-            if isfile(join(run, "simple.csv")): continue
-            if not isfile(join(run, "info.csv")): continue
-            info = pd.read_csv(join(run, "info.csv"))
-            for name in ("csf", "wm", "gm", "prev_1", "prev_2", "next_1", "next_2"):
-                image = np.load(join(run, f"{name}.npy"))
-                info[name] = image[:, 4, 4, 4]
-            info["bold"] = np.load(join(run, "bold.npy"))
-            dfs.append(info)
+    folders = get_folders(root)
+    for f in tqdm(folders):
+        if not isfile(join(f, "info.csv")): continue
+        info = pd.read_csv(join(f, "info.csv"))
+        for name in ("csf", "wm", "gm", "prev_1", "prev_2", "next_1", "next_2"):
+            image = np.load(join(f, f"{name}.npy"))
+            info[name] = image[:, 4, 4, 4]
+        info["bold"] = np.load(join(f, "bold.npy"))
+        dfs.append(info)
     df = pd.concat(dfs)
     df.drop(["height", "weight"], axis=1, inplace=True) # some heights are strings so just ignore for now
     df.reset_index(drop=True, inplace=True)
@@ -28,11 +36,3 @@ if __name__ == "__main__":
     root = args[0]
     collate(root)
 
-"""
-NOTES:
-
-This might be helpful to fix height
-height_map = {"5'9": 175.26, "5'10\"": 177.8, "5'11": 180.34}
-
-Unfortunately conn152 has 60 which might be either 160cm or 60in, both reasonable
-"""
